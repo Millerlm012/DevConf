b8979098038ca0bf51df23faa9c5ba209dd534b5
Landon Miller <millerlm012@gmail.com>
2024-03-10 21:37:51 -0500

    Updated naming convention

--- install/unix.sh
+++ install/unix.sh
@@ -1,47 +1,43 @@
 #/bin/bash
 
 # cloning repo to /tmp
-if [ -d /tmp/DevConf ]; then
-  rm -rf /tmp/DevConf
+if [ -d /tmp/dotfiles ]; then
+	rm -rf /tmp/dotfiles
 fi
-git clone https://github.com/Millerlm012/DevConf /tmp/DevConf
-
+git clone https://github.com/Millerlm012/dotfiles /tmp/dotfiles
 
 # making config dir and sub dirs
 if [ ! -d ~/.config ]; then
-  mkdir ~/.config
+	mkdir ~/.config
 fi
 
 if [ ! -d ~/.config/alacritty ]; then
-  mkdir ~/.config/alacritty
+	mkdir ~/.config/alacritty
 fi
-cp -r /tmp/DevConf/config/alacritty/ ~/.config/alacritty/
-
+cp -r /tmp/dotfiles/config/alacritty/ ~/.config/alacritty/
 
 if [ ! -d ~/.config/tmux ]; then
-  mkdir ~/.config/tmux
+	mkdir ~/.config/tmux
 fi
-cp -r /tmp/DevConf/config/tmux ~/.config/tmux/
-
+cp -r /tmp/dotfiles/config/tmux ~/.config/tmux/
 
 if [ ! -d ~/.config/nvim ]; then
-  mkdir ~/.config/nvim
-  git clone https://github.com/LazyVim/starter ~/.config/nvim
+	mkdir ~/.config/nvim
+	git clone https://github.com/LazyVim/starter ~/.config/nvim
 else
-  echo "nvim dir already exists, creating a backup and installing lazyvim"
-  current_date_time=$(date +"%Y%m%d_%T")
-  if [ -d ~/.config/nvim ]; then
-    mv ~/.config/nvim{,_$current_date_time.bak}
-  fi
-  if [ -d ~/.local/share/nvim ]; then
-	  mv ~/.local/share/nvim{,_$current_date_time.bak}
-  fi
-  if [ -d ~/.local/state/nvim ]; then
-    mv ~/.local/state/nvim{,_$current_date_time.bak}
-  fi
-  if [ -d ~/.cache/nvim ]; then
-    mv ~/.cache/nvim{,_$current_date_time.bak}
-  fi
-  git clone https://github.com/LazyVim/starter ~/.config/nvim
+	echo "nvim dir already exists, creating a backup and installing lazyvim"
+	current_date_time=$(date +"%Y%m%d_%T")
+	if [ -d ~/.config/nvim ]; then
+		mv ~/.config/nvim{,_$current_date_time.bak}
+	fi
+	if [ -d ~/.local/share/nvim ]; then
+		mv ~/.local/share/nvim{,_$current_date_time.bak}
+	fi
+	if [ -d ~/.local/state/nvim ]; then
+		mv ~/.local/state/nvim{,_$current_date_time.bak}
+	fi
+	if [ -d ~/.cache/nvim ]; then
+		mv ~/.cache/nvim{,_$current_date_time.bak}
+	fi
+	git clone https://github.com/LazyVim/starter ~/.config/nvim
 fi
-
