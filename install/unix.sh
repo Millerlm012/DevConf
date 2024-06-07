8a69fd75b170abe5375bd0fe0f362f75628a056e
Landon Miller <millerlm012@gmail.com>
2024-06-07 13:29:05 -0500

    Updated install

--- install/unix.sh
+++ install/unix.sh
@@ -11,21 +11,15 @@ if [ ! -d ~/.config ]; then
 	mkdir ~/.config
 fi
 
-if [ ! -d ~/.config/alacritty ]; then
-	mkdir ~/.config/alacritty
-fi
-cp -r /tmp/dotfiles/.config/alacritty/ ~/.config/
+cp -r /tmp/dotfiles/.config/alacritty ~/.config
+cp -r /tmp/dotfiles/.config/tmux ~/.config
 
-if [ ! -d ~/.config/tmux ]; then
-	mkdir ~/.config/tmux
-fi
-cp -r /tmp/dotfiles/.config/tmux ~/.config/
 # source tmux conf
 tmux source ~/.config/tmux/tmux.conf
 
 if [ ! -d ~/.config/nvim ]; then
-	mkdir ~/.config/nvim
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	# git clone https://github.com/LazyVim/starter ~/.config/nvim
+	cp -r /tmp/dotfiles/.config/nvim ~/.config
 else
 	echo "nvim dir already exists, creating a backup and installing lazyvim"
 	current_date_time=$(date +"%Y%m%d_%T")
@@ -41,5 +35,6 @@ else
 	if [ -d ~/.cache/nvim ]; then
 		mv ~/.cache/nvim{,_$current_date_time.bak}
 	fi
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	# git clone https://github.com/LazyVim/starter ~/.config/nvim
+	cp -r /tmp/dotfiles/.config/nvim ~/.config
 fi
